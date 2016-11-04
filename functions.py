bc8d5885f4c2d7b60a4ec244a4a3ca384bcf6078
aliang8 <aliangdw@gmail.com>
2016-11-04 14:37:49 -0400

    Add files via upload

--- functions.py
+++ functions.py
@@ -18,15 +18,15 @@ def initializeTables():
 #========================================================GENERIC CREATE FUNCTIONS=============================================================
 #Need to add functions that handles all these inputs
 def newAccount(username, password):
-	hashpass = hashlib.sha512(password).hexdigest()
-	c.execute("INSERT INTO accounts VALUES('%s', %s)" % (username, hashpass))
+    hashpass = hashlib.sha512(password).hexdigest()
+    c.execute("INSERT INTO accounts VALUES('%s', %s)" % (username, hashpass))
 
-	def newStory(title, storyid, content, contributor, timestamp):
-		c.execute("INSERT INTO entries VALUES(%s, '%s', %s, '%s', %s)" % (storyid, content, 1, contributor, timestamp))
-		c.execute("INSERT INTO stories VALUES(%s, '%s')" % (storyid, title))
+def newStory(title, storyid, content, contributor, timestamp):
+    c.execute("INSERT INTO entries VALUES(%s, '%s', %s, '%s', %s)" % (storyid, content, 1, contributor, timestamp))
+    c.execute("INSERT INTO stories VALUES(%s, '%s')" % (storyid, title))
 
-		def newEntry(storyid, content, entrynum, contributor):
-			c.execute("INSERT INTO entries VALUES(%s, '%s', %s, '%s', %s)" % (storyid, content, entrynum, contributor, timestamp))
+def newEntry(storyid, content, entrynum, contributor):
+    c.execute("INSERT INTO entries VALUES(%s, '%s', %s, '%s', %s)" % (storyid, content, entrynum, contributor, timestamp))
 #=============================================================================================================================================
 
 
@@ -34,27 +34,27 @@ def newAccount(username, password):
 
 #Returns a list of all posts a user has contributed to
 def returnContributed(username):
-	data = c.execute("SELECT DISTINCT storyid FROM entries WHERE entries.contributor == %s ORDER BY timestamp ASC" % (username))
-	stories = []
-	for item in data:
-		stories.append(item[0])	#Item[0] = storyid
-	return stories
-	
+    data = c.execute("SELECT DISTINCT storyid FROM entries WHERE entries.contributor == %s ORDER BY timestamp ASC" % (username))
+    stories = []
+    for item in data:
+	stories.append(item[0])	#Item[0] = storyid
+    return stories
+    
 #Returns one entire story as a string
 def returnStory(storyid):
-	data = c.execute("SELECT * FROM entries WHERE entries.storyid == %s ORDER BY entrynum ASC" % (storyid))
-	story = []
-	for item in data:
+    data = c.execute("SELECT * FROM entries WHERE entries.storyid == %s ORDER BY entrynum ASC" % (storyid))
+    story = []
+    for item in data:
 	story.append(item[1]) # Item 1 = Story content of one entry
-	return story
+    return story
 
 #Returns a list of all contributors to a story in order
 def returnContributors(storyid):
-	data = c.execute("SELECT * FROM entries WHERE entries.storyid == %s ORDER BY entrynum ASC" % (storyid))
-	contributors = []
-	for item in data:
+    data = c.execute("SELECT * FROM entries WHERE entries.storyid == %s ORDER BY entrynum ASC" % (storyid))
+    contributors = []
+    for item in data:
 	contributors.append(item[3]) #Item[3] = contributor
-	return contributors
+    return contributors
 
 #==============================================================================================================================================
 
@@ -69,23 +69,49 @@ def returnContributors(storyid):
 # Tuple represented in components : ([], [][], [][])
 # For my account/my stories page
 def myStoryList(username):
-	myStories = returnContributed(username)
-	allStories = []
-	allContributors = []
-	allTitles = []
+    myStories = returnContributed(username)
+    allStories = []
+    allContributors = []
+    allTitles = []
 
-	for storyid in myStories:
-		allStories.append(returnStory(storyid))
-		allContributors.append(returnContributors(storyid))
+    for storyid in myStories:
+	allStories.append(returnStory(storyid))
+	allContributors.append(returnContributors(storyid))
 
-		data = c.execute("SELECT * FROM stories WHERE stories.storyid == %s" % (storyid))
+    	data = c.execute("SELECT * FROM stories WHERE stories.storyid == %s" % (storyid))
 	allTitles.append(data.fetchone()[1]) #First (and only) entry fetch. fetch[1] = title
 	
-	return (allTitles, allStories, allContributors)
+    return (allTitles, allStories, allContributors)
 
 #Returns the list of stories for the main page
 #UNFINISHED
 def menuStories(numStories):
-	#data = c.execute("SELECT ")
+    #data = c.execute("SELECT ")
 
 #=============================================================================================================================================
+
+def signin(username,password):
+    hashpass = hashlib.sha24(password).hexdigest()
+    users = c.execute("SELECT pass FROM users WHERE user == %s" % (username))
+    if users and users[0] == hashpass:
+        return True
+    else:
+        return False
+
+def register(username,password):
+    user = c.execute("SELECT user FROM users WHERE user == %s" % (username))
+    if user:
+        return 1
+    elif len(username) < 3 and len(password) < 3:
+        return 2
+    elif len(password) < 3
+        return 3
+    elif len(username) < 3
+        return 4
+    elif not(username.isalum()) or not(password.isalum()):
+        return 5
+    else:
+        c.execute("INSERT INTO users VALUES (%s,%s)" % (username,password))
+        return 6
+
+
